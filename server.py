8ff1b70fd15837e7cf10ddcdefe14f88c03b0b68
Peter <pesomerville@peter-somervilles-macbook-pro.local>
2018-04-16 16:58:39 +0800

    getting close, need to pretty up

--- server.py
+++ server.py
@@ -7,6 +7,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/planes')
+def planes():
+    return render_template('planes.html')
 
+@app.route('/trains')
+def trains():
+    return render_template('trains.html')
+
+@app.route('/automobiles')
+def automobiles():
+    return render_template('automobiles.html')
+
+@app.route('/boats')
+def boats():
+    return render_template('boats.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 app.run(debug=True)
